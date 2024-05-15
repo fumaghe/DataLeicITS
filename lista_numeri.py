a1ca2b64dacab1dab2019312bdf14e9f8a592e1b
Andrea Fumagalli <andrea.fumagalli@itsrizzoli.it>
2024-05-15 12:41:19 +0200

    Modifica nomi variabili

--- lista_numeri.py
+++ lista_numeri.py
@@ -1,13 +1,13 @@
 # Esercizio lista numeri
 
 
-min = int(input("Dimmi il numero minore: "))
-max = int(input("Dimmi il numero maggiore: "))
+uas = int(input("Dimmi il numero minore: "))
+sata = int(input("Dimmi il numero maggiore: "))
 
-for i in range(min, max+1):
+for i in range(uas, sata+1):
 	if not i%7: 
 		print('Buzz')
 	else: 
 		print(i) 
 
-print(sum(range(min, max + 1)))
+print(sum(range(uas, sata + 1)))

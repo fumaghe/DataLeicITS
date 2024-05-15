aa8f483cb9f74850845c5279f6e83220f5c51e36
Pierluigi Durante <pierluigi.durante@itsrizzoli.it>
2024-05-15 12:25:55 +0200

    Inserito stampa numeri

--- lista_numeri.py
+++ lista_numeri.py
@@ -2,3 +2,11 @@
 
 min = int(input("Dimmi il numero minore: "))
 max = int(input("Dimmi il numero maggiore: "))
+
+for i in range(min, max+1):
+	if not i%7: 
+		print('Buzz')
+	else: 
+		print(i) 
+
+	
